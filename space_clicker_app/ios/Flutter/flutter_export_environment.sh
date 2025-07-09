3488c017ab5b9db9daf0b91af6afc09a2cccf1a1
Dimitri <dimitri.brancourt@gmail.com>
2025-07-09 14:13:17 +0200

    preparation de l'installateur

--- space_clicker_app/ios/Flutter/flutter_export_environment.sh
+++ space_clicker_app/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\dimit\Documents\Flutter\flutter_windows_3.32.5-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\dimit\Documents\ynov\B3\SpaceClicker\space_clicker_app"
+export "FLUTTER_ROOT=C:\Users\Utilisateur\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Utilisateur\Documents\travail\ynov\B3\SpaceClicker\space_clicker_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
